be750ebbbdd27ac5b63af2ea9cd57427ba295905
MengxuanCai <mengxuanccc@gmail.com>
2016-10-17 15:35:27 -0700

    quickSort Project

---
+++ code/quickSortProject/QuickSort.java
@@ -0,0 +1,47 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class QuickSort here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class QuickSort extends Actor
+{
+    /**
+     * Act - do whatever the QuickSort wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int pivot;
+    Message m = new Message();
+    Number[] number;
+    public QuickSort(Number[] numbers) {
+        Set<Number> numbers = new HashSet<>();
+        while(set.size() < 8) {
+            set.add(new Number((int) Math.random() * 20);
+        }
+    
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    } 
+    //pick pivot in array
+    public int pickPivot(){
+        
+    }    
+    //move pivot to the position
+    public int movePivot() {
+    }
+    //check if the number fits the position
+    public boolean check() {
+    }
+    //check if it is the right time to end
+    public boolean caculate(int start, int end) {
+    }
+    //check error times
+    public int errorCount() {
+    }
+
+    
+}
---
+++ code/quickSortProject/QuickSortWorld.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class QuickSortWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public QuickSortWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        this.prepare();
+    }
+    private void Prepare(){
+        
+        
+    }
+}
