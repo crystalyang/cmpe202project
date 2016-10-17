ff73ddeba46419c6f1bc59ff0a062764dbd4a5d0
EmmaPro <tingting.wang@sjsu.edu>
2016-10-17 15:19:35 -0700

    image number change

---
+++ code/quickSortAction/code/Judge.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Judge here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Judge extends Actor
+{
+    /**
+     * Act - do whatever the Judge wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    private int countError=0;
+    private int start,end;
+    
+    public void act() 
+    {
+        // Add your action code here.
+        
+            
+    }  
+    
+    //to checke the position, the countError, whether it is down
+    public void check(){
+        
+        
+    
+    
+    }
+    
+    
+    
+}
